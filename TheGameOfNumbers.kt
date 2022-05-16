6ffce36cc03e034a8a137c198871beef5aadbe0b
mTifiy <105554653+mTifiy@users.noreply.github.com>
2022-05-17 00:16:09 +0300

    Update TheGameOfNumbers.kt

--- TheGameOfNumbers.kt
+++ TheGameOfNumbers.kt
@@ -1,3 +1,21 @@
+fun main(){
+    // These constants set the game perimeters
+    val maxGameNumber = 10
+    val minGameNumber = 0
+    
+    val game = TheGame()
+    
+    /*
+     * Befor you start the game, please, remember the "Key words":
+     * "yes", "no", "bigger" and "smaller"
+     */
+    while(game.doTouWantToPlay()){
+    game.playerGuesses(minGameNumber, maxGameNumber)
+    game.computerGuesses(minGameNumber,maxGameNumber)
+    }
+}
+
+
 /**
  * this cass serialises the game.
  * The game consists of two parts.
@@ -6,122 +24,78 @@
  * In nte second part of the game the computer mast guess the number
  * with you have picked
  */
-
-class TheGameOfNumbers {
-    // These constants set the game perimeters
-    private val maxGameNumber = 100
-    private val minGameNumber = 0
-
+class TheGame{
+    
+    /**
+     * This function ask the player wil him play the game
+     * if the player sey "Yes" the function return "true"
+     * if the player sey "No" the function return "false"
+     */
+    public fun doTouWantToPlay(): Boolean {
+        while (true) {
+            when (js("prompt(\"Do you wont to play? yes/no\")")) {
+                "no" -> return false
+                "yes" -> return true
+                else -> println("i don't understand you =(")
+            }
+        }
+    }
+    
+    
     /**
      * This function is responsible for the second part of the game
      * A player enter a number. If the number is bigger or smaller
      * the function informs the payer.
      * If the player has guest the number, the function is over
      */
-    private fun playerGuesses(minNumber: Int = 0, maxNumber: Int = 100){
-
-
-        val hidingNumber = (minNumber..maxNumber).random()
-
-        var answer = enterNumber()
-
+    public fun playerGuesses(minNumber: Int = 0, maxNumber: Int = 100){
+        
+        val hidingNumber: Int = (minNumber..maxNumber).random()
+        
+        var answer: Int  = js("+prompt(\"Guess the number: \")")
+        
         while (hidingNumber != answer) {
-            if (hidingNumber > answer) println("The number is bigger")
-            else println("The number is smaller")
-            answer = enterNumber()
+            if (hidingNumber > answer) answer = js("+prompt(\"The number is bigger\")")
+            else answer = js("+prompt(\"The number is smaller\")")
         }
-        println("You guessed it!\n")
+        js("alert(\"You guessed it\")")
     }
-
+    
+    
     /**
      * This function is responsible for the first part of the game
      * A computer enter a number. If the number is bigger or smaller
      * the payer informs the computer.
      * If the computer has guest the number, the function is over
      */
-    private fun computerGuesses(minNumber: Int = 0, maxNumber: Int = 100) {
+    public fun computerGuesses(minNumber: Int = 0, maxNumber: Int = 100) {
 
-
-        var min = minNumber
-        var max = maxNumber
+        js("alert(\"In the game use these words to guide me: yes, bigger, smaller\")")
+        var min: Int = minNumber
+        var max: Int = maxNumber
         var answer: Int
 
         label@ while (true) {
 
             if (min > max) {
-                println("This is impossible!\n")
+                js("alert(\"This is impossible!\")")
                 break
             }else if (min == max) {
-                println("$min is your number!")
+                js("alert(\"This is yor number: \" + min)")
                 break
             }
 
             answer = (min..max).random()
 
-            when (askTheAnswer(answer)) {
+            when (js("prompt(\"Is it yor number?: \" + answer)")) {
                 "bigger" -> min = answer + 1
                 "smaller" -> max = answer - 1
                 "yes" -> {
-                    println("Yay, I guessed the number! =)\n")
+                    js("alert(\"Yay, I guessed the number! =)\")")
                     break@label
-                }
-                else -> println("i don't understand you =(")
+                } else -> js("alert(\"i don't understand you =(\")")
             }
         }
     }
-
-    /**
-     * This function return the number from the payer
-     */
-    private fun enterNumber(): Int {
-        print("Guess the number: ")
-        return readLine()!!.toInt()
-    }
-    /**
-     * This function return the answer from the payer
-     */
-    private fun askTheAnswer(number: Int): String {
-        println("Is it yor number: $number ? ")
-        return readLine().toString()
-    }
-
-    /**
-     * This function ask the player wil him play the game
-     * if the player sey "Yes" the function return "true"
-     * if the player sey "No" the function return "false"
-     */
-    private fun doTouWantToPlay(): Boolean {
-        println("Do you wont to play? Yes/No ")
-        while (true) {
-            when (readLine().toString()) {
-                "no" -> return true
-                "yes" -> return false
-                else -> println("i don't understand you =(")
-            }
-        }
-    }
-
-    companion object {
-        /**
-         * This function make the game
-         */
-        @JvmStatic
-        fun main(args: Array<String>) {
-            // create the object of the class
-            val myGame = TheGameOfNumbers()
-
-            while (true) {
-
-                if (myGame.doTouWantToPlay()) break
-
-                println("let me guess the number \nUse these words to guide me: yes, bigger, smaller")
-                myGame.computerGuesses(myGame.minGameNumber, myGame.maxGameNumber)
-
-                if (myGame.doTouWantToPlay()) break
-
-                println("It's your turn to guess the number")
-                myGame.playerGuesses(myGame.minGameNumber, myGame.maxGameNumber)
-            }
-        }
-    }
+    
 }
